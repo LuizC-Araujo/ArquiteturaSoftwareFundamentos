5d06e294c76d34932da6fac7f7173c565e6c90cb
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-02 12:04:11 -0300

    abstracao

---
+++ FundamentosArquitetura/02 - Pilares OOP/03 - Abstracao.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP
+{
+    // superclasse - abestração
+    // só pode ser herdada, nunca instanciada, devido ao abstract
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        // não é obrigado a implementar quando tem abstract
+        public abstract void Ligar();
+        public abstract int Desligar();
+    }
+}
