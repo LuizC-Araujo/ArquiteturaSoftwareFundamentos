0320c528aac4ea685e0b1b7d2e6c716a2e708faa
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-02 11:42:14 -0300

    classes x objetos

---
+++ FundamentosArquitetura/01 - Classes x Objetos/Casa.cs
@@ -0,0 +1,27 @@
+﻿namespace OOP;
+
+// Definição de Classe
+public class Casa
+{
+    // Representa os dados de qualquer coisa do mundo real
+    public int TamanhoM2 { get; set; }
+    public int Andares { get; set; }
+    public decimal Valor { get; set; }
+    public int NumeroVagas { get; set; }
+}
+
+public class Objeto
+{
+    // Definição de Objeto
+    public Objeto()
+    {
+        // Uma instância da classe
+        var casa = new Casa
+        {
+            TamanhoM2 = 100,
+            Andares = 3,
+            Valor = 100000,
+            NumeroVagas = 2
+        };
+    }
+}
