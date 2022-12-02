d67b9ab33a6bd55596c2514cd8738df9187abbd2
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-02 12:00:57 -0300

    herança

---
+++ FundamentosArquitetura/02 - Pilares OOP/02 - Heranca.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP
+{
+    // Funcionario É uma Pessoa
+    public class Funcionario : Pessoa
+    {
+        public DateTime DataAdmissao { get; set; }
+        public string Registro { get; set; }
+    }
+
+    public class Processo
+    {
+        public void Execucao()
+        {
+            var funcionario = new Funcionario
+            {
+                Name = "John Doe",
+                DataNascimento = Convert.ToDateTime("1990/01/01"),
+                DataAdmissao = DateTime.Now,
+                Registro = "651515"
+            };
+
+            funcionario.CalcularIdade();
+        }
+    }
+}
