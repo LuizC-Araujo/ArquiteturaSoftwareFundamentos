1ea4642210ff243994e74bb8e56b2ed70eb42fc0
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-02 11:51:17 -0300

    estado e comportamento

---
+++ FundamentosArquitetura/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP
+{
+    // Estado é representado pelas propriedades
+    public class Pessoa
+    {
+        public string Name { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        // Comportamento é representado pelo método gerando ou
+        // processando uma informação através da própria classe
+        // altera o estado da entidade
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual.Year < DataNascimento.Year)
+                idade--;
+
+            return idade;
+        }
+    }
+}
