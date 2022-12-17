31f1fb43c0b7e09a22a2a6a4e1a562cb392a0d54
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-17 17:40:23 -0300

    encapsulamento

---
+++ FundamentosArquitetura/02 - Pilares OOP/05 - Encapsulamento.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP;
+
+public class AutomacaoCafe
+{
+    // esconder comportamentos privados e expor outros que faça uso desses comportamentos
+    // fazer uso de um método sem saber de fato como ele funciona
+    public void ServirCafe()
+    {
+        var expresso = new CafeteiraExpressa();
+        expresso.Ligar();
+        expresso.PrepararCafe();
+        expresso.Desligar();
+    }
+}
