ecd993d678693e916cb99a351a7270c8f6af2cf5
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-20 17:32:39 -0300

    violação da SRP

---
+++ Solid/1 - SRP - Single Responsability Principle/SRP.Violacao/Cliente.cs
@@ -0,0 +1,57 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace Solid.SRP.Violacao
+{
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public string AdicionarCliente()
+        {
+            if (!Email.Contains("@"))
+                return "Cliente com e-mail inválido";
+
+            if (CPF.Length != 11)
+                return "Cliente com CPF inválido";
+
+            using (var cn = new SqlConnection())
+            {
+                var cmd = new SqlCommand();
+                
+                cn.ConnectionString = "MyConnection";
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO CLIENTE (NOME, EMAIL, CPF, DATACADASTRO) VALUES (@nome, @email, @cpf, @dataCadastro";
+
+                cmd.Parameters.AddWithValue("nome", Nome);
+                cmd.Parameters.AddWithValue("email", Email);
+                cmd.Parameters.AddWithValue("cpf", CPF);
+                cmd.Parameters.AddWithValue("dataCadastro", DataCadastro);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            var mail = new MailMessage("bart@simpsom.com", Email);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mail.Subject = "Bem Vindo.";
+            mail.Body = "Você está cadastrado.";
+            client.Send(mail);
+
+            return "Cliente cadastrado com sucesso";
+        }
+    }
+}
