63d0fab7a1e6a9d258e07b8f2c3ba8d9fc44d5ec
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2022-12-19 22:18:06 -0300

    modificadores de acesso e comportamento

---
+++ ProjetoA/Classes.cs
@@ -0,0 +1,97 @@
+﻿using System.Runtime.CompilerServices;
+
+//[assembly: InternalsVisibleTo("ProjetoB")] //Dessa forma internal funciona no outro projeto
+namespace ProjetoA
+{
+    #region Classes
+    public class Publica
+    {
+        public void TestePublico() { }
+        private void TestePrivado() { }
+        internal void TesteInternal() { }
+        protected void TesteProtegido() { }
+        private protected void TestePrivadoProtegido() { }
+        protected internal void TesteProtegidoInterno() { }
+    }
+
+    public sealed class Selada { } // classes seladas não podem ser herdadas
+
+    class Privada { } // sem modificador de acesso é privada
+
+    internal class Interna { }
+
+    abstract class Abstrata { }
+
+    #endregion
+
+    #region Testes
+
+    class TesteClasses
+    {
+        public TesteClasses()
+        {
+            var publica = new Publica();
+            var privada = new Privada();
+            var interna = new Interna();
+            //var abstrata = new Abstrata(); não pode ser instanciada
+        }
+    }
+    //class TesteSelada : Selada { } não pode ser instanciada
+
+    class TesteModificador1
+    {
+        public TesteModificador1()
+        {
+            var publica = new Publica();
+
+            publica.TestePublico();
+            publica.TesteInternal(); // dentro do mesmo assembly
+            publica.TesteProtegidoInterno(); // está internos dentro do mesmo assembly
+            //publica.TesteProtegido(); não consigo chamar pq não estou herdando da classe
+            //publica.TestePrivadoProtegido(); deve estar herdando
+            //publica.TestePrivado(); método privado não pode ser chamado
+        }
+    }
+
+    class TesteModificador2 : Publica
+    {
+        public TesteModificador2()
+        {
+            TestePublico();
+            TesteInternal();
+            TesteProtegido();
+            TesteProtegidoInterno();
+            TestePrivadoProtegido();
+            //TestePrivado(); não conseguimos chamar, nem herdando
+        }
+    }
+
+    #endregion
+}
+
+/**********************************************************/
+// public:
+// Acesso não é restrito
+/**********************************************************/
+
+// protected
+// Acesso é limitado a classe que contém o tipo derivado da classe
+/**********************************************************/
+
+// internal
+// Acesso é limitado apenas ao assembly - dll
+/**********************************************************/
+
+// protected internal - para métodos
+// Limitadas ao assmbly ou derivadas dessas classes
+/**********************************************************/
+
+// private
+// Acesso limitado apenas para a classe
+/**********************************************************/
+
+// private protected - para métodos
+// acesso é limitado apenas paras as classes que derivam ou
+// se estão no mesmo assembly - dispon[ivel desde o C# 7.2
+/**********************************************************/
+
---
+++ ProjetoB/Classes.cs
@@ -0,0 +1,55 @@
+﻿using ProjetoA;
+
+namespace ProjetoB
+{
+    class TesteClasses
+    {
+        public TesteClasses()
+        {
+            var publica = new Publica();
+            //var privada = new Privada(); // não posso pq é privada / com a linha no projetoA eu consigo
+            //var interna = new Interna(); // não posso pq não faz parte do mesmo assembly / com a linha no projetoA eu consigo
+            //var abstrata = new Abstrata(); não posso pq é abstrata
+        }
+    }
+
+    class TesteModificador1
+    {
+        public TesteModificador1()
+        {
+            //aqui também só consigo acessar o método público
+            var publica = new Publica();
+            
+            publica.TestePublico();
+
+            // pelo interno tenho acessos a dois métodos a mais
+
+            //publica.TesteInternal();
+            //publica.TesteProtegidoInterno();
+
+            // esses não consigo acessar 
+
+            //publica.TesteProtegido();
+            //publica.TestePrivadoProtegido();
+            //publica.TestePrivado();
+        }
+    }
+
+    class TesteModificador2 : Publica
+    {
+        public TesteModificador2()
+        {
+            // herdando do outro assembly eu consigo os três métodos:
+
+            TestePublico();
+            TesteProtegido();
+            TesteProtegidoInterno();
+
+            // esses não consigo acessar 
+
+            //TesteInternal();
+            //TestePrivadoProtegido(); / para esse nível deve ser uma herança e estar no mesmo assembly
+            //TestePrivado();
+        }
+    }
+}
