82c3516f83d0fc21f97a31ee8ccb9527d643aede
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2023-01-24 11:37:42 -0300

    feature: solucao 1 ocp

---
+++ Solid/2 - OCP = Open Closed Principle/OCP.Solucao/DebitoContaInvestimento.cs
@@ -0,0 +1,12 @@
+﻿namespace Solid.OCP.Solucao
+{
+    public class DebitoContaInvestimento : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // Debito Conta Investimento
+            // Isentar Taxas
+            return FormatarTransacao();
+        }
+    }
+}
---
+++ Solid/2 - OCP = Open Closed Principle/OCP.Solucao/DebitoContaPoupanca.cs
@@ -0,0 +1,12 @@
+﻿namespace Solid.OCP.Solucao
+{
+    public class DebitoContaPoupanca : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // Valida aniversário da conta
+            // Debita Conta Poupança
+            return FormatarTransacao();
+        }
+    }
+}
