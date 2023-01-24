e7988e23284056dad3f3e1fda5798259a5d33088
Luiz Carlos Araujo <luiz.silva@conferironline.com.br>
2023-01-24 12:39:27 -0300

    feature: lsp violacao

---
+++ Solid/3 - LSP - Liskov Substitution Principle/LSP.Violacao/CalculaArea.cs
@@ -0,0 +1,27 @@
+﻿namespace Solid.LSP.Violacao
+{
+    public class CalculaArea
+    {
+        private static void ObterAreaRetangulo(Retangulo ret)
+        {
+            Console.Clear();
+            Console.WriteLine("Cálculo da área do Retângulo");
+            Console.WriteLine();
+            Console.WriteLine($"{ret.Altura} * {ret.Largura}");
+            Console.WriteLine();
+            Console.WriteLine(ret.Area);
+            Console.ReadKey();
+        }
+
+        public static void Calcular()
+        {
+            var quad = new Quadrado()
+            {
+                Altura = 10,
+                Largura = 5
+            };
+
+            ObterAreaRetangulo(quad);
+        }
+    }
+}
